e4aa3343685984992878de60b908694ccf2848ee
Gabriel Hazi <hazigabriel@gmail.com>
2020-05-13 01:47:14 +0300

    added html, css and some js variables

---
+++ script.js
@@ -0,0 +1,10 @@
+let generateGridButton =  document.getElementById('generateGridButton');
+let eraseGridButton =  document.getElementById('eraseGridButton');
+let colorOnButton = document.getElementById("colorOnButton");
+let monochromeOnButton = document.getElementById("monochromeOnButton")
+
+
+
+monochromeOnButton.onclick = function(){
+	alert()
+}
