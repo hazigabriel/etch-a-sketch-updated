29bd48889277850ce933cfea4040a3e6b9457f26
Gabriel Hazi <hazigabriel@gmail.com>
2020-05-16 15:20:57 +0300

    Final commit, cleaned code, added comments and implemented monochrome function

--- script.js
+++ script.js
@@ -10,45 +10,59 @@ let colorLabel = document.getElementById("colorLabel");
 let monochromeLabel = document.getElementById("monochromeLabel");
 let gradientLabel =	document.getElementById("gradientLabel");
 
+//generate a random number between 0 and 256, for our colorMode
+function randomRGBColor() {
+	return Math.trunc((Math.random() * 256))
+};
+
+//calls the randomRGBColor function 3 times, and each RGB value receives random number between 1 and 256, resets opacity to full
+//if the MonochromeMode function was used to hover the element previously
+
 let colorMode = function() {
 		document.querySelectorAll(".gridBlock").forEach(function(divElelement){
 		divElelement.addEventListener("mouseenter", function(e){
 			e.target.style.background = "rgb(" + randomRGBColor() +","+ randomRGBColor() +","+ randomRGBColor() +")" ;
 			e.target.style.border = "none";
+			e.target.style.opacity = "1";
 		})
 	})
 }
-let blackMode = function() {
+
+///define the opacity for the monochrom fade function
+let op = 0;
+//add this.grid block element an additional 0.1 opacity with each color, and sets its color to black
+let monochromeMode = function() {
 	document.querySelectorAll(".gridBlock").forEach(function(divElelement){
+		let opIncrement = 0;
 		divElelement.addEventListener("mouseenter", function(e){
+			if(opIncrement < 1){
+					opIncrement += 0.1;
+			}
+			e.target.style.opacity = op + opIncrement;
 			e.target.style.background = "black" ;
 			e.target.style.border = "none";
 		})
 	})
 };
+ 
+ 
+
 
 monochromeOnButton.onclick = function(){
-	blackMode();
+	monochromeMode();
 	///add the "active" class to the selected butotn, and remove it from the other buttons
 	colorLabel.classList.remove("active");
 	monochromeLabel.classList.add("active");
-	gradientLabel.classList.remove("active");
 }
 colorOnButton.onclick = function(){
 	colorMode();
 	///add the "active" class to the selected butotn, and remove it from the other buttons
 	colorLabel.classList.add("active");
 	monochromeLabel.classList.remove("active");
-	gradientLabel.classList.remove("active");
-}
-gradientOnButton.onclick = function() {
-
-	///add the "active" class to the selected butotn, and remove it from the other buttons
-	colorLabel.classList.remove("active");
-	monochromeLabel.classList.remove("active");
-	gradientLabel.classList.add("active");
 }
 
+
+//start with color mode by default
 colorMode()
 
 //take the user input, if all conditions are met generate a grid, with a relative block sizes
@@ -63,7 +77,7 @@ userInputSubmit.onclick = function(){
 		gridArea.innerHTML = ""; //delete previous div elements
 		for(let i = 0; i < userInput.value * userInput.value; i++) {
 			let newDiv = document.createElement("div");
-			newDiv.style.cssText = "height:" + (640 / userInput.value) + "px; width:" + (640 / userInput.value) +"px;";
+			newDiv.style.cssText = "height:" + (640 / userInput.value) + "px; width:" + (640 / userInput.value) +"px;"; 
 			newDiv.classList.add("gridBlock");
 			gridArea.appendChild(newDiv);
 
@@ -74,18 +88,12 @@ userInputSubmit.onclick = function(){
 	if(colorLabel.classList.contains("active")) {
 		colorMode();
 	} else if (monochromeLabel.classList.contains("active")) {
-		blackMode();
-	} else {
-		alert("gradiento")
+		monochromeMode();
 	}
 }
 
-//generate a random number between 0 and 256, for our colorMode
-function randomRGBColor() {
-	return Math.trunc((Math.random() * 256))
-};
 
-
+//add original styling to each grid block element
 eraseGridButton.onclick = function() {
 	let currentDiv = document.querySelectorAll(".gridBlock");
 	for(i = 0; i < currentDiv.length; i++) {
